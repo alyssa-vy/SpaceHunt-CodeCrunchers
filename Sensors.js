b4b6e71d4c2e9a8dcf73105e80f0000c3a2662f6
alyssa-vy <56896185+alyssa-vy@users.noreply.github.com>
2019-11-09 13:38:01 -0800

    Added Log and Sensors Button
    Created a textarea for the log (might change to some kind of form) and added a button for using sensors.

---
+++ Sensors.js
@@ -0,0 +1,9 @@
+// Story: As a player, I want to see what is located at nearby Celestial Points, so I know where things are.
+// Acceptance Criteria:
+// 1. Verify that if the player deploys sensors for the current CP, 2% of the supplies are consumed.
+// 2. Verify that celestial objects within two CP of the current CP are displayed
+// 3. Verify that celestial objects within two CP of the current CP are added to the Celestial M
+
+function deploySensor() {
+    alert("Deploying Sensors")
+}
