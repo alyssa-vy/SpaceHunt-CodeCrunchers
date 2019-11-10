f1b64ecff71a3fde779fa94ade627aa778c0e123
Joseph Jesse <jjesse@pdx.edu>
2019-11-10 14:45:56 -0800

    Began working on movement
    Added the position class to the SpaceCraftMovement.js file, gave it x and y coordinates for its fields, added setters and getters for those private fiels, and have just now begun working on the moveSpacecraft function.

--- SpaceCraftMovement.js
+++ SpaceCraftMovement.js
@@ -1,5 +1,36 @@
-function moveSpacecraft(angle,distance) {
-   alert("Move Space Craft");
-   checkEnergy();
-   checkSupplies();
+//Global vairbale for the max size of the map deminsion.
+const max = 128;
+
+//Nested position object within the spacecraft object representing
+//its current position at any given time.
+const position = {
+    _x: 0,  //X coordinate for the position of the spacecraft
+    _y: 0,  //Y coordinate for the position of the spacecraft
+
+    //Setter and getter functions since the field for this class
+    //are private.
+    get x() { return this._x; },
+    get y() { return this._y; },
+    set x(num) { this._x = num; },
+    set y(num) { this._y = num; },
+
+    moveSpacecraft(angle, distance) {
+        //Error checking to make sure that the values passes in for 
+        //angle and distance are indeed numbers.
+        if(typeof angle !== 'number' || typeof distance !== 'number' || distance < 1) {
+            alert("Error in attempting to move spacecraft.\nPlease provide valid values for the angle and distance when attempting to move.");
+            return false;
+        }
+
+        if(angle === 0) {
+            if((x + distance) >= max) {
+                //Do call a wormhole function
+            }
+        }
+
+
+        /*alert("Move Space Craft");
+            checkEnergy();
+            checkSupplies();*/
+    }
 }
