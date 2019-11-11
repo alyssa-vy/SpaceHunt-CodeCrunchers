700b93dcdd989b6911d010a0736a4dad68219e28
alyssa-vy <56896185+alyssa-vy@users.noreply.github.com>
2019-11-11 15:37:10 -0800

    Update Sensors.js
    Created ability to append text to the log area.

--- Sensors.js
+++ Sensors.js
@@ -2,10 +2,13 @@
 // Acceptance Criteria:
 // 1. Verify that if the player deploys sensors for the current CP, 2% of the supplies are consumed.
 // 2. Verify that celestial objects within two CP of the current CP are displayed
-// 3. Verify that celestial objects within two CP of the current CP are added to the Celestial M
+// 3. Verify that celestial objects within two CP of the current CP are added to the Celestial Map
 
 function addToLog() {
-    document.getElementById('log').value = "Test";
+    artifact = "Planet" // test celestial artifact
+    x = 1
+    y = 2
+    document.getElementById('log').value += artifact + " detected at celestial point (" + x + ", " + y + ")\n"
 }
 
 function deploySensor() {
