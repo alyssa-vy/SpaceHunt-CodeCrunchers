3995dfd2a60a321c6d9fea1d570ca5d7fb840977
jake-bickle <bickle.jake@gmail.com>
2019-11-10 16:09:04 -0800

    Rework on input validation for the configuration page

--- js/config.js
+++ js/config.js
@@ -8,37 +8,89 @@ var defaultConfig = {
     initialEnergy: 100,
     initialSupplies: 100,
     initialCredits: 1000,
+    godMode: false,
     randomWormholeBehavior: true,
-    canFail: true
 };
 
 function getConfig(){
     config = defaultConfig;
-    config.initialLocation.x = configurationSelectors.initialLocationX.value;
-    config.initialLocation.y = configurationSelectors.initialLocationY.value;
-    config.boardWidth = configurationSelectors.boardWidth.value;
-    config.boardHeight= configurationSelectors.boardHeight.value;
-    config.initialEnergy = configurationSelectors.initialEnergy.value;
-    config.initialSupplies= configurationSelectors.initialSupplies.value;
-    config.initialCredits= configurationSelectors.initialCredits.value;
-    if (configurationSelectors.godMode)
-        config.canFail = false;
-    if (configurationSelectors.staticWormholeBehavior)
+    config.initialLocation.x = parseInt(configurationSelectors.initialLocationX.value);
+    config.initialLocation.y = parseInt(configurationSelectors.initialLocationY.value);
+    config.boardWidth = parseInt(configurationSelectors.boardWidth.value);
+    config.boardHeight= parseInt(configurationSelectors.boardHeight.value);
+    config.initialEnergy = parseInt(configurationSelectors.initialEnergy.value);
+    config.initialSupplies= parseInt(configurationSelectors.initialSupplies.value);
+    config.initialCredits= parseInt(configurationSelectors.initialCredits.value);
+    config.godMode = configurationSelectors.godMode.checked
+    if (configurationSelectors.staticWormholeBehavior.checked)
         config.randomWormholeBehavior = false;
-    console.log(config);
-
     return config;
 }
 
 function validateNumber(){
-    numberInput = event.target;
+    numberInput= event.target;
     min = parseInt(numberInput.min)
     max = parseInt(numberInput.max)
+    if (numberInput.value === "" || isNaN(numberInput.value)){
+        setInvalid(numberInput, "Please enter a whole number");
+        return;
+    }
     currentValue = parseInt(numberInput.value)
     if (currentValue < min){
-        numberInput.value = numberInput.min;
+        setInvalid(numberInput, "Number must be greater than " + min);
     }
     else if (currentValue > max){
-        numberInput.value = numberInput.max;
+        setInvalid(numberInput, "Number must be less than " + max);
+    }
+    else{
+        setValid(numberInput);
+    }
+}
+
+function validateCoordinate(coordinate){
+    
+}
+
+function isInt(string){
+    if (isNaN(string))
+        return false;
+    return (string.includes(".") && !string.endsWith("."))
+}
+
+function setInvalid(inputField, message){
+    inputField.classList.add("invalidInput");
+    errorMessageElement = inputField.parentElement.getElementsByClassName("invalidInputMessage")[0];
+    errorMessageElement.innerHTML = message;
+}
+
+function setValid(inputField){
+    inputField.classList.remove("invalidInput");
+    errorMessageElement = inputField.parentElement.getElementsByClassName("invalidInputMessage")[0];
+    errorMessageElement.innerHTML = "";
+}
+
+function setConfigurationDefault(){
+    configurationSelectors.initialLocationX.value = defaultConfig.initialLocation.x
+    configurationSelectors.initialLocationY.value = defaultConfig.initialLocation.y
+    configurationSelectors.boardWidth.value = defaultConfig.boardWidth
+    configurationSelectors.boardHeight.value = defaultConfig.boardHeight
+    configurationSelectors.initialEnergy.value = defaultConfig.initialEnergy
+    configurationSelectors.initialSupplies.value = defaultConfig.initialSupplies
+    configurationSelectors.initialCredits.value = defaultConfig.initialCredits
+    configurationSelectors.godMode.checked = defaultConfig.godMode
+    configurationSelectors.staticWormholeBehavior.checked = !defaultConfig.randomWormholeBehavior
+    inputFields = configurationSelectors.querySelectorAll("input");
+    inputFields.forEach(function(inputField){
+        setValid(inputField);
+    });
+}
+
+function disableSubmitIfInvalid(){
+    invalidInputFields = document.getElementById("configuration").getElementsByClassName("invalidInput");
+    if (invalidInputFields.length > 0){
+        submitButton = document.getElementById("configurationSubmitButton").disabled = true;
+    }
+    else{
+        submitButton = document.getElementById("configurationSubmitButton").disabled = false;
     }
 }
