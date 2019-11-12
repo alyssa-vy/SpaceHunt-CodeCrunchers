2129da968c0188fedeff042dc6249a577502f048
cmaxwell2 <56896619+cmaxwell2@users.noreply.github.com>
2019-11-12 15:44:27 -0800

    Update Resources.js with true/false returns in all functions for testing

--- Resources.js
+++ Resources.js
@@ -15,25 +15,31 @@ const resources = {
 			/*if (!godMode){
 				//call gameover function
 			}*/
+			return false;
 		}
+		return true;
 	},
 	
 	function addEnergy(toAdd) {
 		let evaledAdd = eval(toAdd);
 		if (evaledAdd < 0){
 			alert ("Do not call addEnergy with negative value - call subtractEnergy to lower energy");
+			return false;
 		}
 		_energy += evaledAdd;
 		document.UI.energy.value = _energy;
+		return true;
 	},
 	
 	function subtractEnergy(toSubtract) {
 		let evaledSubtract = eval(toSubtract);
 		if (evaledSubtract < 0){
 			alert ("Do not call subtractEnergy with negative value - call with positive value to be subtracted");
+			return false;
 		}
 		_energy -= evaledAdd;
 		document.UI.energy.value = _energy;
+		return true;
 	},
 
 	function checkSupplies() {
@@ -52,23 +58,28 @@ const resources = {
 		let evaledAdd = eval(toAdd);
 		if (evaledAdd < 0){
 			alert ("Do not call addSupplies with negative value - call subtractSupplies to lower supplies");
+			return false;
 		}
 		_supplies += evaledAdd;
 		document.UI.supplies.value = _supplies;
+		return true;
 	},
 	
 	function subtractSupplies(toSubtract) {
 		let evaledSubtract = eval(toSubtract);
 		if (evaledSubtract < 0){
 			alert ("Do not call subtractSupplies with negative value - call with positive value to be subtracted");
+			return false;
 		}
 		_supplies -= evaledAdd;
 		document.UI.supplies.value = _supplies;
+		return true;
 	},
 	
 	function subtractSuppliesTwo() {
 		_supplies -= 2;
 		document.UI.supplies.value = _supplies;
+		return true;
 	}
 };
 
