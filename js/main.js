740fa35bc3a80fd7e95e24748735f7e44710a06b
jake-bickle <bickle.jake@gmail.com>
2019-11-16 11:59:13 -0800

    Add some testing code for the worldCanvis object

--- js/main.js
+++ js/main.js
@@ -1,10 +1,27 @@
-function main(){
+function main() {
     initConfig();
     position.initPosition();
-    //initMap();
 }
 
-function initGame(){
+function initGame() {
     resources.initResources();
     position.initPosition();
+    m = new Map(config.boardWidth, config.boardHeight);
+
+    c = new worldCanvis(config.boardWidth, config.boardHeight);
+    // This code to bind keys is temporary. Don't rely on it being here for long.
+    document.addEventListener('keydown', function(event) {
+        if (event.code == 'ArrowRight') {
+            c.moveEast("player", 1);
+        }
+        if (event.code == 'ArrowDown') {
+            c.moveSouth("player", 1);
+        }
+        if (event.code == 'ArrowLeft') {
+            c.moveWest("player", 1);
+        }
+        if (event.code == 'ArrowUp') {
+            c.moveNorth("player", 1);
+        }
+    }.bind(c));
 }
