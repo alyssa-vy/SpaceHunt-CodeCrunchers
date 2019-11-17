2982adb2520e74651c9e0e835f064a56717626a1
jake-bickle <bickle.jake@gmail.com>
2019-11-16 16:49:08 -0800

    Update main to reflect recent canvis changes

--- js/main.js
+++ js/main.js
@@ -1,14 +1,13 @@
 function main() {
     initConfig();
-    position.initPosition();
 }
 
 function initGame() {
     resources.initResources();
+    worldCanvis.initCanvis();
     position.initPosition();
     m = new MapClass(config.boardWidth, config.boardHeight);
 
-    c = new worldCanvis(config.boardWidth, config.boardHeight);
 
     // baddy code and binds are only temporary. They won't be here for long.
 
@@ -16,19 +15,19 @@ function initGame() {
     baddy.src = "img/letter_b.jpg";  // Add an image asset
     baddy.classList.add("enemy");   // Describe what type of object this is
     baddy.id = "baddy-1";  // All canvas objects require an id. If you don't give it an id, the canvas can't retrieve it
-    c.addToCanvas(baddy, 0, 0);  // Send the element to canvas, which will put it in the corresponding coordinates
+    worldCanvis.addToCanvas(baddy, 0, 0);  // Send the element to canvas, which will put it in the corresponding coordinates
 
     baddy2 = document.createElement("img");
     baddy2.src = "img/letter_b.jpg";
     baddy2.classList.add("enemy");
     baddy2.id = "baddy-2";  // All Ids must be unique
-    c.addToCanvas(baddy2, 10, 0);
+    worldCanvis.addToCanvas(baddy2, 10, 0);
 
-    c.movePlayerNorth(1);  // Move the player North by 1 cell
-    c.moveNorth("player", 1);  // This is equivalent to the above call
-    c.moveWest("baddy-1",1);  // Move baddy-1 one unit west
-    c.moveSouth("baddy-2",10);  // Move baddy-2 south 10 units
-    c.updateImage("player", "img/letter_c.jpg")  // You can update the graphic of any element on the canvas,
+    worldCanvis.movePlayerNorth(1);  // Move the player North by 1 cell
+    worldCanvis.moveNorth("player", 1);  // This is equivalent to the above call
+    worldCanvis.moveWest("baddy-1",1);  // Move baddy-1 one unit west
+    worldCanvis.moveSouth("baddy-2",10);  // Move baddy-2 south 10 units
+    worldCanvis.updateImage("player", "img/letter_c.jpg")  // You can update the graphic of any element on the canvas,
                                                  // This may be useful for simple animation
 
     document.addEventListener('keydown', function(event) {
@@ -44,7 +43,6 @@ function initGame() {
         if (event.code == 'ArrowUp') {
             c.moveNorth("player", 1);
         }
-    }.bind(c));
-    position.initPosition();
+    }.bind());
     switchToPage("mainGame");
 }
