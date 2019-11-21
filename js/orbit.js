0f70c26de4747a7f89bcee382f2d7997cc3d12e4
alyssa-vy <56896185+alyssa-vy@users.noreply.github.com>
2019-11-20 18:44:08 -0800

    Update orbit.js
    added test confirm boxes for entering and leaving orbit

--- js/orbit.js
+++ js/orbit.js
@@ -1,3 +1,20 @@
-function enterOrbit() {
+// When encountering a planet, a user is given the option to enter it's orbit which takes 1 turn and reduces supplies.
 
+function enterOrbit(planet) {
+    if (confirm("Would you like to enter " + planet + "'s orbit?'")) {
+        resources.subtractSuppliesTwo();
+        if (resources.checkSupplies()) {
+
+        }
+    }
+
+}
+
+function leaveOrbit(planet) {
+    if (confirm("Would you like to leave " + planet + "'s obrit?'")) {
+        resources.subtractSuppliesTwo();
+        if (resources.checkSupplies()) {
+
+        }
+    }
 }
