0ec1a98525e3ffed63ab42668395d898747ddd8a
jake-bickle <bickle.jake@gmail.com>
2019-11-24 13:56:13 -0800

    Create artifacts.js, a file that contains all of the game's board objects

---
+++ js/artifacts.js
@@ -0,0 +1,25 @@
+var artifacts = {
+    "Pentium-1": new Planet("Pentium-1", "img/pentium_1.jpg"),
+    "Pentium-2": new Planet("Pentium-2", "img/pentium_2.jpg"),
+    "Pentium-3": new Planet("Pentium-3", "img/pentium_3.jpg"),
+    "Pentium-4": new Planet("Pentium-4", "img/pentium_4.jpg"),
+    "Pentium-5": new Planet("Pentium-5", "img/pentium_5.jpg"),
+    "Pentium-6": new Planet("Pentium-6", "img/pentium_6.jpg"),
+    "Pentium-7": new Planet("Pentium-7", "img/pentium_7.jpg"),
+    "Ryzen": new Planet("Ryzen", "img/ryzen.jpg"),
+    "Celeron": new Planet("Celeron", "img/celeron.jpg"),
+    "Xeon": new Planet("Xeon", "img/xeon.jpg"),
+    "Asteroid": undefined,  // asteroid object has not been made yet
+    "Spacestation": undefined, // spacestation object has not been made yet
+    "Badmax": undefined, // badmax object has not been made yet
+    "Wormhole": undefined // wormhole object has not been made yet
+}
+
+// This function sets all the variables for each planet within artifacts
+function initializeAllPlanets(){
+
+}
+
+function randomizeStrongboxPlacement(){
+
+}
