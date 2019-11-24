482216644693cd3bb8cf312dea38fb7deb75b8c7
jake-bickle <bickle.jake@gmail.com>
2019-11-24 13:59:01 -0800

    initializeMap now creates an object for worldMap

--- js/initializeMap.js
+++ js/initializeMap.js
@@ -1,26 +1,7 @@
-var Map = null;
+var worldMap = null;
 
 function initializeMap() {
-    /*
-        Initializes the global "Map" variable as a 2D array
-        using the  configuration information on the previous page
-        and then switches to the "artifactPlacement" page in the HTML
-        using the switchToPage method,
-    */
-
-    const rows = config.boardHeight;
-    const columns = config.boardWidth;
-    Map = new Array(columns);
-    for(let i = 0; i < columns; ++i){
-        Map[i] = new Array(rows);
-    }
-
-    for(let x = 0; x < columns; ++x){
-        for(let y = 0; y < rows; ++y){
-            Map[x][y] = null;
-        }
-    }
-
+    worldMap = new Map(config.boardWidth, config.boardHeight);
     switchToPage("artifactPlacement");
-    return Map;
+    return worldMap;
 }
