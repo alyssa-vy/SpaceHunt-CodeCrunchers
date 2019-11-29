6f3985d5367e60a93bec468cfbd1a87a0c86ea71
tobinshields <TobinShields@live.com>
2019-11-29 15:26:37 -0800

    Update assets

--- js/initPlanets.js
+++ js/initPlanets.js
@@ -1,14 +1,14 @@
 var planets = {
-    "Pentium-1": new Planet("Pentium-1", "img/pentium_1.jpg"),
-    "Pentium-2": new Planet("Pentium-2", "img/pentium_2.jpg"),
-    "Pentium-3": new Planet("Pentium-3", "img/pentium_3.jpg"),
-    "Pentium-4": new Planet("Pentium-4", "img/pentium_4.jpg"),
-    "Pentium-5": new Planet("Pentium-5", "img/pentium_5.jpg"),
-    "Pentium-6": new Planet("Pentium-6", "img/pentium_6.jpg"),
-    "Pentium-7": new Planet("Pentium-7", "img/pentium_7.jpg"),
-    "Ryzen": new Planet("Ryzen", "img/ryzen.jpg"),
-    "Celeron": new Planet("Celeron", "img/celeron.jpg"),
-    "Xeon": new Planet("Xeon", "img/xeon.jpg"),
+    "Pentium-1": new Planet("Pentium-1", "img/pentium_1.png"),
+    "Pentium-2": new Planet("Pentium-2", "img/pentium_2.png"),
+    "Pentium-3": new Planet("Pentium-3", "img/pentium_3.png"),
+    "Pentium-4": new Planet("Pentium-4", "img/pentium_4.png"),
+    "Pentium-5": new Planet("Pentium-5", "img/pentium_5.png"),
+    "Pentium-6": new Planet("Pentium-6", "img/pentium_6.png"),
+    "Pentium-7": new Planet("Pentium-7", "img/pentium_7.png"),
+    "Ryzen": new Planet("Ryzen", "img/ryzen.png"),
+    "Celeron": new Planet("Celeron", "img/celeron.png"),
+    "Xeon": new Planet("Xeon", "img/xeon.png"),
 }
 
 // This function sets all the variables for each planet within artifacts
