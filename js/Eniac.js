e2cf2d1e4199d4eedf32f675f01093243a10e56e
alyssa-vy <56896185+alyssa-vy@users.noreply.github.com>
2019-12-01 18:30:06 -0800

    Update Eniac.js
    corrected typos

--- js/Eniac.js
+++ js/Eniac.js
@@ -5,12 +5,12 @@ class Eniac extends CelestialArtifact {
 
     interact() {
         alert("You have entered Eniac.");
-        if (!resources.hasStrongbox) {
+        if (!resources.hasStrongBox) {
             alert("Come back with the strongbox containing the ancient recipe for Koca-Kola in order to gain a zillion credits!");
         }
         else {
             alert("You've successfully returned the ancient recipe for Koca-Kola! The marketing department of the Koca-Kola company has rewarded you with a zillion credits!");
-            Resources.addCredits(10000000);
+            resources.addCredits(10000000);
             gameWin();
         }
     }
